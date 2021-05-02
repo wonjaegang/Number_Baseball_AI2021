676eb88cf0e3dd12dab32affc536aef74e960492
Wonjaegang <robinsam@naver.com>
2021-05-02 14:30:08 +0900

    Add Umpire & Pitcher class
    Umpire class will save target number and answer to each player's question. Pitcher class will receive target number and question from user or AI.
    Also, I added Game_settings.py For easy setting change.

--- Number_baseball_main.py
+++ Number_baseball_main.py
@@ -1,5 +1,28 @@
 import AI_Pitcher
+import Game_settings
+
+
+class Umpire:
+    def __init__(self):
+        self.digits = Game_settings.digits
+        self.target_num = []
+
+
+class Pitcher:
+    def __init__(self, order):
+        self.order = order
+        self.digits = Game_settings.digits
+
+        print("Enter player%d's object number." % order)
+        self.object_num = input()
+        umpire.target_num.append(self.object_num)
 
 
 if __name__ == "__main__":
-    pass
+    umpire = Umpire()
+    player1 = Pitcher(1)
+    player2 = Pitcher(2)
+
+    gameOn = True
+    while gameOn:
+        pass
